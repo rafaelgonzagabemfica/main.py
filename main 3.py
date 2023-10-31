47c00087a3cc8afb3b33b6a839aec386a672e15d
rafaelgonzagabemfica <132838590+rafaelgonzagabemfica@users.noreply.github.com>
2023-10-31 16:24:37 -0300

    Add files via upload

---
+++ main 3.py
@@ -0,0 +1,15 @@
+n1 = int(input("tamanho a?"))
+n2= int(input("tamanho b?"))
+if n1<n2:
+    menor = n1
+else :
+    menor = n2
+
+n3= int(input("tamanho c?"))
+if n3<menor:
+    print("p menor umero e:",menor)
+else:
+    print("p menor umero e:",menor)
+
+
+
