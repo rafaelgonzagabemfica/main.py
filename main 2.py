dc88bcf73189bf4ff96d9463ff043b2d19228a0d
rafaelgonzagabemfica <132838590+rafaelgonzagabemfica@users.noreply.github.com>
2023-10-31 16:15:48 -0300

    Add files via upload

---
+++ main 2.py
@@ -0,0 +1,11 @@
+tam_a = int(input("tamanho a?"))
+tam_b = int(input("tamanho b?"))
+tam_c = int(input("tamanho c?"))
+
+if tam_a> tam_b+tam_c
+print("isto e um triangulo")
+
+else
+print(tam_a)
+print(tam_b)
+print(tam_c)
